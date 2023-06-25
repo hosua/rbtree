7b0ca00a8f492760bd74959d795cfe16f770e9ff
hosua <hosua.o@gmail.com>
2023-06-25 02:28:25 -0400

    tree.rb: Some cleaning up
    Cleaned up some stuff

--- tree.rb
+++ tree.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/ruby
 
 require 'colorize'
+require 'optparse'
 
-dir_path = ""
-
-if ARGV.length == 0
-  dir_path = "."
-else
-  dir_path = ARGV[0]
-end
-
+# ternary operator in ruby
+ARGV.length == 0 ? (dir_path = ".") : (dir_path = ARGV[0])
 
 class TreeNav
   def initializer
@@ -24,18 +19,19 @@ class TreeNav
   def show(dir)
     chdir(dir)
     def list_folder(dir, depth=1, vis=[])
-      prefix = "├──"
-      prefix_last = "└──"
-      prefix_stem = "│   "
+      PREFIX = "├── "
+      PREFIX_LAST = "└── "
+      PREFIX_STEM = "│   "
+      
+      # pre will either be set to PREFIX or PREFIX_LAST depending
+      # on if it's the last element in its directory.
+      pre = PREFIX 
       vis.push(dir)
       padding = ""
-      
-      pre = prefix
       entries = Dir.glob("*")
 
       for x in 1..depth-1 do
-        padding += prefix_stem
-        # padding += "──"
+        padding += PREFIX_STEM
       end
 
       entries.each_with_index do |e, idx|
@@ -44,7 +40,7 @@ class TreeNav
           # ignore relative paths as well.
           if !vis.include? e and e != '.' and e != '..'
             # print the directory name
-            puts "#{padding}#{pre}" + " #{e}".colorize(:light_magenta)
+            puts "#{padding}#{pre}" + "#{e}".colorize(:light_magenta)
             # "push" the upper dir to the stack
             chdir(e)
             list_folder(e, depth+1, vis)
@@ -52,14 +48,17 @@ class TreeNav
             chdir('..')
           end
         else
+          # print the L-angle connector piece since we are at the bottom of the "branch"
           if idx == entries.length - 1
-            pre = prefix_last
+            pre = PREFIX_LAST
           end
           # print the file name
           if File.executable? e
-            puts "#{padding}#{pre}" + " #{e}".colorize(:light_green)
+            # color the executable files green
+            puts "#{padding}#{pre}" + "#{e}".colorize(:light_green)
           else
-            puts "#{padding}#{pre} #{e}"
+            # leave the other files white
+            puts "#{padding}#{pre}#{e}"
           end
         end
       end
@@ -71,6 +70,9 @@ class TreeNav
 
   attr_accessor :cwd
 end
+
+
+
 
 t = TreeNav.new.show(dir_path)
 
